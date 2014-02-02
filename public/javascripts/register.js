0bdf7de4eb8e5450b1be2a687212788c7be0305d
frank bassard <f.bassard@gmail.com>
2014-02-02 14:36:43 +0100

    Bootstraping the alert colors, forms : register

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -9,18 +9,22 @@ var setWarning = function(label){
 var INIT = {
 	setEvent : function(){
 		$('#login').blur(function(){
+			$('#login-group').removeClass('has-error');
 			var login = $('#login').val();
 			$.post("/register/loginAvailable", {login : login}, function(data){
 				if(data[0].number !== 0){
+					$('#login-group').addClass('has-error');
 					setWarning('Login already in use.');
 				}
 			}, "json").fail(function() {});
 		});
 
 		$('#mail').blur(function(){
+			$('#mail-group').removeClass('has-error');
 			var mail = $('#mail').val();
 			$.post("/register/mailAvailable", {mail : mail}, function(data){
 				if(data[0].number !== 0){
+					$('#mail-group').addClass('has-error');
 					setWarning('mail already in use.');
 				}
 			}, "json").fail(function() {});
